0d2875cc0be95eb9001c4d1920dbc13706e6517a
nico <nicolasoterobouza@gmail.com>
2026-04-18 18:10:25 +0200

    Module info changed

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,8 +5,10 @@ module edu.nob.liceo.triviado_proyectofin_nicolasotero {
     requires org.hibernate.orm.envers;
     requires java.sql;
     requires org.hibernate.orm.core;
+    requires java.naming;
 
     opens edu.nob.liceo.triviado_proyectofin_nicolasotero to javafx.fxml;
+    opens edu.nob.liceo.triviado_proyectofin_nicolasotero.controllers to javafx.fxml;
     opens edu.nob.liceo.triviado_proyectofin_nicolasotero.model to org.hibernate.orm.core, jakarta.persistence;
     opens edu.nob.liceo.triviado_proyectofin_nicolasotero.util to org.hibernate.orm.core, jakarta.persistence;
     exports edu.nob.liceo.triviado_proyectofin_nicolasotero;
